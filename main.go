5b8d0701f40f3b81c410661894da6e8a7b81d8d9
Louis Chemineau <artonge.c@gmail.com>
2017-07-22 20:26:07 +0300

    Fix static media serve

--- main.go
+++ main.go
@@ -5,26 +5,20 @@ import (
 
 	"github.com/OpenTransports/Paris/api"
 	"github.com/go-siris/siris"
-	"github.com/go-siris/siris/context"
 )
 
 func main() {
 	// Create new siris app
 	app := siris.New()
 
-	// Serve web client
-	app.Get("/", func(ctx context.Context) {
-		ctx.ServeFile("./public/index.html", false)
-	})
-	assetHandler := app.StaticHandler("./public", false, false)
-	app.SPA(assetHandler)
+	// Serve medias files
+	app.StaticWeb("/medias", "./medias")
 
 	// Build api
-	apiRoute := app.Party("/api")
 	// /api/transports?latitude=...&longitude=...
-	apiRoute.Get("/transports", api.GetTransports)
+	app.Get("/transports", api.GetTransports)
 	// /api/agencies?latitude=...&longitude=...
-	apiRoute.Get("/agencies", api.GetAgencies)
+	app.Get("/agencies", api.GetAgencies)
 
 	// Set listening port
 	var port = os.Getenv("PORT")
